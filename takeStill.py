07186126f707c7e93af335e89276bf95604f2a57
aktham2 <andrewktham@gmail.com>
2018-05-20 18:11:51 -0500

    Delete takeStill.py

--- takeStill.py
+++
@@ -1,8 +0,0 @@
-import picamera 
-
-def takeStill(filename):
-	camera = picamera.PiCamera()
-	camera.vflip = True
-	fileName = 'pictures/' + str(filename) + '.jpg'
-	camera.capture(fileName)
-	return 0
